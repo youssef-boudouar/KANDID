a570dad75096cfda5253e06a312d447cea883863
youssef-boudouar <youssefboudouar771@gmail.com>
2026-03-26 11:28:25 +0100

    feat: add Job Offers CRUD routes to API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\JobOfferController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    // Job Offers CRUD
+    Route::get('/job-offers', [JobOfferController::class, 'index']);
+    Route::post('/job-offers', [JobOfferController::class, 'store']);
+    Route::get('/job-offers/{id}', [JobOfferController::class, 'show']);
+    Route::put('/job-offers/{id}', [JobOfferController::class, 'update']);
+    Route::delete('/job-offers/{id}', [JobOfferController::class, 'destroy']);
 });
+
