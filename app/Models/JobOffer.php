b3b0edd94e70a24048f47b4379f4c1aa9ca7b800
youssef-boudouar <youssefboudouar771@gmail.com>
2026-03-26 11:10:01 +0100

    feat: create JobOffer model with relationships and fillable attributes

---
+++ app/Models/JobOffer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JobOffer extends Model
+{
+    protected $fillable = [
+        'company_id',
+        'created_by',
+        'title',
+        'description',
+        'status',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'job_offer_tag');
+    }
+}
