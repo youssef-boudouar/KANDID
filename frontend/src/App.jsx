4b2e184b458877ac078567cf192eb09999e00f2a
youssef-boudouar <youssefboudouar771@gmail.com>
2026-04-01 10:28:55 +0100

    feat: add route for create job offer page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import JobOffers from './pages/JobOffers';
+import CreateJob from './pages/CreateJob';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
@@ -16,7 +17,7 @@ function App() {
                 {/* Recruiter only routes */}
                 {/* <Route path="/dashboard" element={<Dashboard />} /> */}
                 <Route path="/job-offers" element={<JobOffers />} />
-                {/* <Route path="/job-offers/create" element={<CreateJob />} /> */}
+                <Route path="/job-offers/create" element={<CreateJob />} />
 
                 {/* Unknown URL*/}
                 <Route path="*" element={<Navigate to="/login" />} />
