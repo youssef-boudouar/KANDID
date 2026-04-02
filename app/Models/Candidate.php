9226fbef8586e71d03519f8d0c02600f0649b04f
youssef-boudouar <youssefboudouar771@gmail.com>
2026-04-02 10:23:21 +0100

    feat: create Candidate model

---
+++ app/Models/Candidate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Candidate extends Model
+{
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'resume_path',
+    ];
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
+}
