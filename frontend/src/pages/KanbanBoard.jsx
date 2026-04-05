bf34869b4653ae1c229f64bf014aba51f4e86dd9
youssef-boudouar <youssefboudouar771@gmail.com>
2026-04-05 18:29:14 +0100

    feat: add API call to apply kanban moves updates to database

--- frontend/src/pages/KanbanBoard.jsx
+++ frontend/src/pages/KanbanBoard.jsx
@@ -2,53 +2,65 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-
-function KanbanBoard()
-{
-    const status = ['screening', 'interview', 'technical', 'hired', 'rejected'];
+function KanbanBoard() {
+    const status = ["screening", "interview", "technical", "hired", "rejected"];
     const [applications, setApplications] = useState([]);
-    const {id} = useParams();
+    const { id } = useParams();
     const [loading, setLoading] = useState(true);
-    const [jobTitle, setJobTitle] = useState('');
+    const [jobTitle, setJobTitle] = useState("");
 
-    useEffect(()=>{
-        const token = localStorage.getItem('token');
-        axios.get(`http://localhost:8000/api/job-offers/${id}`, {
-            headers : { Authorization : `Bearer ${token}`}
-        }).then(response => {
-            setJobTitle(response.data.title)
-        })
-        axios.get(`http://localhost:8000/api/job-offers/${id}/applications`, {
-            headers: {Authorization: `Bearer ${token}`}
-        }).then(response => {
-            setApplications(response.data);
-        })
-    },[])
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        axios
+            .get(`http://localhost:8000/api/job-offers/${id}`, {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            .then((response) => {
+                setJobTitle(response.data.title);
+            });
+        axios
+            .get(`http://localhost:8000/api/job-offers/${id}/applications`, {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            .then((response) => {
+                setApplications(response.data);
+            });
+    }, []);
 
     const getByStatus = (status) => {
-        return applications.filter(app => app.status === status);
+        return applications.filter((app) => app.status === status);
     };
 
     const onDragEnd = (result) => {
-        if(!result.destination) return; // if the drag was out of droppable zones
+        if (!result.destination) return; // if the drag was out of droppable zones
 
-        if(result.source.droppableId === result.destination.droppableId // dropped on same place
-            && result.source.index === result.destination.index)
+        if (
+            result.source.droppableId === result.destination.droppableId && // dropped on same place
+            result.source.index === result.destination.index
+        )
             return;
 
-
-        const updatedApplications = applications.map((app)=>{
-            if(app.id === parseInt(result.draggableId ))
-            {
-                app.status = result.destination.droppableId ;
+        const updatedApplications = applications.map((app) => {
+            if (app.id === parseInt(result.draggableId)) {
+                app.status = result.destination.droppableId;
                 app.kanban_order = result.destination.index;
             }
             return app;
-        })
+        });
 
         setApplications(updatedApplications);
 
-    }
+        axios.put(
+            `http://localhost:8000/api/applications/${result.draggableId}/move`,
+            {
+                status: result.destination.droppableId,
+                kanban_order: result.destination.index,
+            },
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            },
+        );
+    };
     return <div>Kanban Board - {jobTitle}</div>;
 }
 
